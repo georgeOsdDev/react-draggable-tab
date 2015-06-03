48530e2c66ef32fe0e0a48203711fffe31bb5b1b
Takeharu Oshida <takeharu.oshida@mobilus.co.jp>
2015-06-03 13:07:59 +0900

    Add Test for Tab

--- test/components/Tab_spec.js
+++ test/components/Tab_spec.js
@@ -9,10 +9,16 @@ describe('Test of Tab', () => {
   let component;
 
   beforeEach(() => {
-    component = TestUtils.renderIntoDocument(<Tab title={'Hello'}><p>test tab</p></Tab>);
+  });
+
+  it('should have default properties', function () {
+    component = TestUtils.renderIntoDocument(<Tab><p>test tab</p></Tab>);
+    expect(component.props.title).to.be.equal('untitled');
+    expect(component.props.disableClose).to.be.equal(false);
   });
 
   it('Tab works like a scala case class, it just render it\'s children', () => {
+    component = TestUtils.renderIntoDocument(<Tab title={'Hello'} ><p>test tab</p></Tab>);
     const p = TestUtils.findRenderedDOMComponentWithTag(component, 'p');
     expect(React.findDOMNode(p).textContent).to.be.equal('test tab');
   });
