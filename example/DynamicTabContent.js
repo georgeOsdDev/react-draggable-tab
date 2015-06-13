29de51de88a589a2f910145364c177931276fdb6
Takeharu.Oshida <georgeosddev@gmail.com>
2015-06-13 13:30:56 +0900

    Update example

--- example/DynamicTabContent.js
+++ example/DynamicTabContent.js
@@ -2,6 +2,7 @@
 
 import _     from 'lodash';
 import React from 'react/addons';
+
 class DynamicTabContent extends React.Component {
   constructor(props) {
     super(props);
@@ -13,6 +14,7 @@ class DynamicTabContent extends React.Component {
   _handleTextChange(e) {
     this.setState({textValue: e.target.value});
   }
+
   render() {
 
     return (
